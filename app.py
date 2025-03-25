658224f436a437dfb0d7c8ade66e51788e98df49
pearmediallc <chetan.kumar@pearmediallc.com>
2025-03-25 12:49:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -669,6 +669,3 @@ def download_website():
     except Exception as e:
         app.logger.error('Exception occurred: %s', str(e))
         return jsonify({'error': str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
