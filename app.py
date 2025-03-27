80f51bad3926e401df5468a90bfbe456cd558d39
pearmediallc <chetan.kumar@pearmediallc.com>
2025-03-27 10:27:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -424,7 +424,7 @@ def download_assets(url, original_domains=None, replacement_domains=None, save_d
         def download_all_assets():
             # Process all elements with URL attributes
             url_attributes = {
-                'img': ['src', 'data-src'],
+                'img': ['src', 'data-src', 'data-srcset'],
                 'script': ['src'],
                 'link': ['href'],
                 'video': ['src', 'poster'],
