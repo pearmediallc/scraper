2f565741d2a81063dc490c73bde324d9c3a180eb
pearmediallc <chetan.kumar@pearmediallc.com>
2025-03-28 15:10:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -768,4 +768,4 @@ def download_website():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(host="0.0.0.0", port=8000)
